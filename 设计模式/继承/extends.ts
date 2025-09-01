9718ef4c244545aeb64613aebccb14a73541351c
virgo569157 <virgo569157@163.com>
2025-09-01 22:06:56 +0800

    设计模式

---
+++ 设计模式/继承/extends.ts
@@ -0,0 +1,42 @@
+/** ES6 中的继承 **/
+
+// 父类
+class Person {
+  private _name: string;
+  private _age: number;
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+  sayHello() {
+    console.log(
+      `Hello, my name is ${this._name} and I am ${this._age} years old.`
+    );
+  }
+}
+
+// 学生类
+class Student extends Person {
+  private _gender: string;
+  private _score: number;
+  constructor(name: string, age: number, gender: string, score: number) {
+    super(name, age);
+    this._gender = gender;
+    this._score = score;
+  }
+  sayScore() {
+    console.log(`My score is ${this._score}.`);
+  }
+  
+  get gender() {
+    return this._gender;
+  }
+  get score() {
+    return this._score;
+  }
+}
+
+const student = new Student("Alice", 18, "female", 90);
+student.sayHello();
+student.sayScore();
+console.log(student.gender);
